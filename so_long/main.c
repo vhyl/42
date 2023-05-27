b2b5bee171d6e007de8855d0fd21282ea29fedc6
vhyl <vaclavhyl773@gmail.com>
2023-05-27 20:18:51 +0200

    1.0

---
+++ so_long/main.c
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vhyl <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/01 17:57:24 by vhyl              #+#    #+#             */
+/*   Updated: 2023/05/01 17:57:26 by vhyl             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#define WIDTH 512
+#define HEIGHT 512
+#include "so_long.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+static mlx_image_t* image;
+
+int32_t ft_pixel(int32_t r, int32_t g, int32_t b, int32_t a)
+{
+    return (r << 24 | g << 16 | b << 8 | a);
+}
+
+void ft_randomize(void* param)
+{
+	for (int32_t i = 0; i < image->width; ++i)
+	{
+		for (int32_t y = 0; y < image->height; ++y)
+		{
+			uint32_t color = ft_pixel(
+				rand() % 0xFF, // R
+				rand() % 0xFF, // G
+				rand() % 0xFF, // B
+				rand() % 0xFF  // A
+			);
+			mlx_put_pixel(image, i, y, color);
+		}
+	}
+}
+
+void ft_hook(void* param)
+{
+	mlx_t* mlx = param;
+
+	if (mlx_is_key_down(mlx, MLX_KEY_ESCAPE))
+		mlx_close_window(mlx);
+	if (mlx_is_key_down(mlx, MLX_KEY_UP))
+		image->instances[0].y -= 5;
+	if (mlx_is_key_down(mlx, MLX_KEY_DOWN))
+		image->instances[0].y += 5;
+	if (mlx_is_key_down(mlx, MLX_KEY_LEFT))
+		image->instances[0].x -= 5;
+	if (mlx_is_key_down(mlx, MLX_KEY_RIGHT))
+		image->instances[0].x += 5;
+}
+
+int	main(void)
+{
+	mlx_t	*mlx;
+
+	mlx = mlx_init(WIDTH, HEIGHT, "MLX42", true);
+	image = mlx_new_image(mlx, 128, 128);
+	mlx_image_to_window(mlx, image, 0, 0);
+
+	mlx_loop_hook(mlx, ft_randomize, mlx);
+	mlx_loop_hook(mlx, ft_hook, mlx);
+
+	mlx_loop(mlx);
+	mlx_terminate(mlx);
+}
---
+++ so_long/so_long.h
@@ -0,0 +1,6 @@
+#ifndef SO_LONG_H
+#define SO_LONG_H
+
+#include "mlx42/include/MLX42/MLX42.h"
+
+#endif
